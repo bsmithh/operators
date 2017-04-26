f7d08d2cdc2b2b50668e7a95f57d85669278e4b2
Bri Smith <smith4bl@dukes.jmu.edu>
2017-04-26 12:23:05 -0400

    update operator_spec.js

--- js/spec/operators_spec.js
+++ js/spec/operators_spec.js
@@ -1,30 +1,30 @@
-// import code to be tested
-var symbols = require("../index");
+/ import code to be tested
+var operator = require("../index");
 
 describe("A program to demonstrate the concept of symbols", () => {
 
 	it("can perform addition", () => {
 		var x = 10;
 		var y = 5;
-		expect(symbols.addition(x, y)).toBe(15);
+		expect(operator.addition(x, y)).toBe(15);
 	});
 
 	it("can perform substraction", () => {
 		var x = 10;
 		var y = 5;
-		expect(symbols.subtraction(x, y)).toBe(5);
+		expect(operator.subtraction(x, y)).toBe(5);
 	});
 
 	it("can perform division", () => {
 		var x = 10;
 		var y = 5;
-		expect(symbols.division(x, y)).toBe(2)
+		expect(operator.division(x, y)).toBe(2)
 	});
 
 	it("can report the remainder", () => {
 		var x = 39
 		var y = 4
-		expect(symbols.modulus(x, y)).toBe(3)
+		expect(operator.modulus(x, y)).toBe(3)
 	});
 
 	it("can perform complex mathematical operations", () => {
@@ -32,7 +32,7 @@ describe("A program to demonstrate the concept of symbols", () => {
 		var y = 50
 		var z = 3.5
 		var a = 4
-		expect(symbols.math(x, y, z, a)).toBe(10)
-		expect(Math.sqrt(a)).toBe(2)
-	})
+		expect(operator.problem(x, y, z, a)).toBe(10)
+		expect(operator.sqrt(a)).toBe(2)
+	});
 });
